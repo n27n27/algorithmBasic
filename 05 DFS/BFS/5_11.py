3d1253ef79f92c4f112788c6cceee50c624c772e
Rolling Stone <aRollingStone27@gmail.com>
2021-09-04 17:46:15 +0900

    queue 학습

---
+++ 05 DFS/BFS/5_11.py
@@ -0,0 +1,50 @@
+# n x m 크기의 직사각형 미로
+# 시작점 (1, 1), 출구 (n, m)
+# n개의 줄에는 각각 m개의 정수(0 혹은 1)로 미로의 정보가 주어진다. 각각의 수들은 공백 없이 붙어서 입력된다.
+# 시작 칸과 마지막 칸은 항상 1이다.
+
+from collections import deque
+import time
+
+n, m = map(int, input().split())
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input())))
+
+# 이동할 네 방향 정의(상, 하, 좌, 우)
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+# BFS 소스코드
+def bfs(x, y):
+    # 큐(Queue) 구현을 위해 deque 라이브러리 사용
+    queue = deque()
+    queue.append((x, y))    
+    
+    while queue:
+        x, y = queue.popleft()
+        print(x, y)
+        # time.sleep(3)
+        # 현재 위치에서 네 방향으로의 위치 확인
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            # 미로 찾기 공간을 벗어난 경우 무시
+            if nx < 0 or ny < 0 or nx >= n or ny >= m:
+                continue
+            
+            # 괴물이 있는 경우 무시
+            if graph[nx][ny] == 0:
+                continue
+
+            # 해당 노드를 처음 방문하는 경우에만 최단 거리 기록
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
+        print(queue)        
+    # 가장 오른쪽 아래까지의 최단 거리 반환
+    return graph[n - 1][m - 1]
+
+# BFS를 수행한 결과 출력
+print(bfs(0, 0))
